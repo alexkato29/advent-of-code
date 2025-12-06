430da210fca3cb22faae12c9fb9bf569f71f41ff
Alex Katopodis <akbd256@gmail.com>
2025-12-06 09:41:43 -0500

    feat: Day 3

---
+++ 2025/day3/a.py
@@ -0,0 +1,22 @@
+from utils.puzzle_input import get_input, split_lines
+
+
+puzzle_input = get_input(year=2025, day=3)
+banks = puzzle_input.split("\n")
+
+res = 0
+
+for bank in banks:
+	first = 0
+	second = 0
+	for i in range(len(bank)):
+		val = int(bank[i])
+		if val > first and (i != len(bank) - 1):
+			first = val
+			second = int(bank[i + 1])
+			continue
+		if val > second:
+			second = val
+	res += 10 * first + second
+
+print(res)
---
+++ 2025/day3/b.py
@@ -0,0 +1,22 @@
+from utils.puzzle_input import get_dummy_input, get_input, split_lines
+
+#puzzle_input = get_dummy_input()
+puzzle_input = get_input(year=2025, day=3)
+banks = puzzle_input.split("\n")
+
+N_BATTERIES = 12
+res = 0
+
+for bank in banks:
+	batteries = [0] * N_BATTERIES
+	for i in range(len(bank)):
+		val = int(bank[i])
+		start = max(0, N_BATTERIES - (len(bank) - i))
+		for j in range(start, len(batteries)):
+			if val > batteries[j]:
+				batteries[j] = val
+				batteries[j + 1:] = [0] * (len(batteries) - j - 1)
+				break
+	res += int("".join(map(str, batteries)))
+
+print(res)
