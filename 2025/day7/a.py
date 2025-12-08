1834a64cbab0e53e67b92c5fdedfadeb17c04e5d
Alex Katopodis <akbd256@gmail.com>
2025-12-07 19:23:29 -0500

    feat: Day 7

---
+++ 2025/day7/a.py
@@ -0,0 +1,32 @@
+from utils.puzzle_input import get_dummy_input, get_input, split_lines
+
+
+#puzzle_input = get_dummy_input()
+puzzle_input = get_input(year=2025, day=7)
+grid = puzzle_input.split("\n")
+for r in grid:
+	r = list(r)
+
+beams = set()
+for i in range(len(grid[0])):
+	if grid[0][i] == "S":
+		beams.add(i)
+		break
+
+res = 0
+for row in range(2, len(grid), 2):
+	updates = []
+	for col in range(len(grid[row])):
+		if grid[row][col] == "^" and col in beams:
+			beams.remove(col)
+			res += 1
+			l = col - 1
+			r = col + 1
+			if l >= 0:
+				updates.append(l)
+			if r < len(grid[r]):
+				updates.append(r)
+	for u in updates:
+		beams.add(u)
+
+print(res)
---
+++ 2025/day7/b.py
@@ -0,0 +1,20 @@
+from utils.puzzle_input import get_dummy_input, get_input, split_lines
+
+#puzzle_input = get_dummy_input()
+puzzle_input = get_input(year=2025, day=7)
+puzzle_input = puzzle_input.split("\n")
+grid = []
+for row in range(len(puzzle_input) - 1, -1, -1):
+	grid.append(list(puzzle_input[row]))
+
+dp = [[0] * len(r) for r in grid]
+dp[0] = [1] * len(grid[0])
+
+for row in range(1, len(grid)):
+	for col in range(len(grid[row])):
+		if grid[row][col] == ".":
+			dp[row][col] = dp[row - 1][col]
+		elif grid[row][col] == "^":
+			dp[row][col] = dp[row - 1][col - 1] + dp[row - 1][col + 1]
+		elif grid[row][col] == "S":
+			print(dp[row - 1][col - 1] + dp[row - 1][col + 1])
